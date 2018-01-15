1395b83a05482066919749409cc58192abb45d23
Rudolf Chrispens <Chrispens@stud.uni-heidelberg.de>
2018-01-15 11:42:13 +0100

    renamed variables in ex10_6.py

--- Ex10/ex10_6.py
+++ Ex10/ex10_6.py
@@ -2,7 +2,7 @@
 import os
 import pyspark.sql.functions as func
 from pyspark.sql.window import Window
-from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, TimestampType, StringType, FloatType, StructField
 
 
@@ -24,37 +24,37 @@ df3 = StructType([
     StructField("timestamp-gap-start", TimestampType(), True)
 ])
 
-def checkCoherency(spark, name=None, df=None):
-    if not df:
+def checkCoherency(spark, name=None, dataFrame=None):
+    if not dataFrame:
         name = name.replace('/', '-')
         filename = './timeseries/' + name
-        df = spark.read.csv(filename, header=True, schema=df2)
-        df = df.groupBy("Timestamp").agg({"Price": "count"}).filter("count(Price) > 1")
-        #if len(df.collect()) > 0:
-            #print(len(df.collect()))
+        dataFrame = spark.read.csv(filename, header=True, schema=df2)
+        dataFrame = dataFrame.groupBy("Timestamp").agg({"Price": "count"}).filter("count(Price) > 1")
+        if len(dataFrame.collect()) > 0:
+            print("Coherency ", len(dataFrame.collect()))
     return
 
 
-def checkCompleteness(spark, name, time=(24*60)):
-    name = name.replace('/', '-')
-    filename = './timeseries/' + name
-    df = spark.read.csv(filename, header=True, schema=df3)
+def checkCompleteness(spark, filteredName, time=(24 * 60)):
+    filteredName = filteredName.replace('/', '-')
+    filepath = './timeseries/' + filteredName
+    dataFrame = spark.read.csv(filepath, header=True, schema=df3)
 
-    window = Window().partitionBy(func.lit(0)).orderBy(func.col("timestamp-gap-start"))
-    df = df.select("*", func.lag("timestamp-gap-start").over(window).alias("timestamp-gap-end")).drop('price')
-    df = df.filter(df["timestamp-gap-end"].isNotNull() & ((df["timestamp-gap-start"].cast("bigint") - df["timestamp-gap-end"].cast("bigint"))/60 > time))
-    df = df.withColumn("price-series-name", func.lit(name))
-    df = df.select(["price-series-name", "timestamp-gap-start", "timestamp-gap-end"])
-    #if len(df.collect()) > 0:
-        #df.show()
-    return df
+    windowModifier = Window().partitionBy(func.lit(0)).orderBy(func.col("timestamp-gap-start"))
+    dataFrame = dataFrame.select("*", func.lag("timestamp-gap-start").over(windowModifier).alias("timestamp-gap-end")).drop('price')
+    dataFrame = dataFrame.filter(dataFrame["timestamp-gap-end"].isNotNull() & ((dataFrame["timestamp-gap-start"].cast("bigint") - dataFrame["timestamp-gap-end"].cast("bigint"))/60 >= time))
+    dataFrame = dataFrame.withColumn("price-series-name", func.lit(filteredName))
+    dataFrame = dataFrame.select(["price-series-name", "timestamp-gap-start", "timestamp-gap-end"])
+    if len(dataFrame.collect()) > 0:
+        print("Completeness ", len(dataFrame.collect()))
+    return dataFrame
 
 
 def readTables(spark, path):
-    df = spark.read \
+    dataFrame = spark.read \
         .csv(path, sep="\t", header=True, schema=df1, timestampFormat="yyyy-MM-dd'T'HH:mm:ss") \
         .drop('Type')
-    return df
+    return dataFrame
 
 
 def generateTimeseries(df):
@@ -70,15 +70,15 @@ def generateTimeseries(df):
     return unique_df
 
 
-def saveTimeseries(name, D, spark):
+def saveTimeseries(name, dataFrame, spark):
     name = name.replace('/', '-')
     filename = './timeseries/' + name
 
     if os.path.isdir(filename):
-        existing = spark.read.csv(filename, header=True, schema=D.schema)
-        D = D.union(existing)
+        existing = spark.read.csv(filename, header=True, schema=dataFrame.schema)
+        dataFrame = dataFrame.union(existing)
 
-    D.distinct().write.csv(filename, header=True, mode='overwrite')
+    dataFrame.distinct().write.csv(filename, header=True, mode='overwrite')
 
 
 if __name__ == '__main__':
@@ -91,8 +91,8 @@ if __name__ == '__main__':
 
     if initialGeneration is True:
         # create ts
-        df: DataFrame = readTables(spark, "./EC2-prices/prices-ca-central*.txt.gz")  # used the test files
-        time_series = generateTimeseries(df)
+        dataFrame = readTables(spark, "./EC2-prices/prices-ca-central*.txt.gz")  # used the test files
+        time_series = generateTimeseries(dataFrame)
         for key, frame in time_series.items():
             saveTimeseries(key, frame, spark)
 
